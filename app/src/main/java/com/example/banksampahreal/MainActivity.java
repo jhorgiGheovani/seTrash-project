b72874483b2e8cbe0068fe5744a53153ce69a3dc
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-08 14:26:21 +0700

    membuat menu login dan sign up

---
+++ app/src/main/java/com/example/banksampahreal/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.banksampahreal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        textView=(TextView) findViewById(R.id.signup);
+        textView.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "You change the page", Toast.LENGTH_SHORT).show();
+            }
+        }));
+    }
+}
